8aac2119b5b2990790d1998a673843b7b807eac6
熊俊杰 <junjie.sop@gmail.com>
2025-05-08 20:05:19 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,6 @@ function Header() {
                 <Link className="px-8 py-5 block" to="/">Home</Link>
                 <Link className="px-8 py-5 block" to="/about">About</Link>
                 <Link className="px-8 py-5 block" to="/contact">Contact</Link>
-                <a href="https://www.youtube.com/channel/UCmcXVCxNwW5rp-KAc8qadbQ"
-                   className="flex gap-2 items-start justify-start">
-                    <span>Youtube</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
-                    </svg>
-                </a>
             </nav>
         </div>
     )
